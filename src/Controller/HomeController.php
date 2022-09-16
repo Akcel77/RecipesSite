f0ad82dace3efb5f3f0894d06a1fa8f3d6489a6a
Akcel77 <Akeucel@gmail.com>
2022-09-16 16:32:57 +0200

    EntityManager acces from HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    /**
+     * @param $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
